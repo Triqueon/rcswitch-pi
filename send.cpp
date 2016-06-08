3efe215ab3d00e95daa389b86d13d59fdc505c09
Pascal Stein <pascal.stein@gmail.com>
2016-06-08 17:37:51 +0200

    Added support for multiple protocols to the send.cpp file. this adds a first parameter.

--- send.cpp
+++ send.cpp
@@ -8,32 +8,67 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    
+
     /*
      output PIN is hardcoded for testing purposes
      see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      for pin mapping of the raspberry pi GPIO connector
      */
     int PIN = 0;
-    char* systemCode = argv[1];
-    int unitCode = atoi(argv[2]);
-    int command  = atoi(argv[3]);
-    
+    RCSwitch mySwitch = RCSwitch();
+    mySwitch.enableTransmit(PIN);
+
+    int protocol = atoi(argv[1]);
+    mySwitch.setProtocol(protocol);
     if (wiringPiSetup () == -1) return 1;
-	printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
-	RCSwitch mySwitch = RCSwitch();
-	mySwitch.enableTransmit(PIN);
-    
-    switch(command) {
-        case 1:
-            mySwitch.switchOn(systemCode, unitCode);
-            break;
-        case 0:
-            mySwitch.switchOff(systemCode, unitCode);
-            break;
-        default:
-            printf("command[%i] is unsupported\n", command);
-            return -1;
+    if (argc < 4) {
+        printf("insufficient number of arguments, please give at least 4 arguments");
+        return 2;
     }
+
+    switch (protocol) {
+        case 1:
+        case 2:
+            char* systemCode = argv[2];
+            int unitCode = atoi(argv[3]);
+            int command  = atoi(argv[4]);
+
+            printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
+            switch(command) {
+                case 1:
+                    mySwitch.switchOn(systemCode, unitCode);
+                    break;
+                case 0:
+                    mySwitch.switchOff(systemCode, unitCode);
+                    break;
+                default:
+                    printf("command[%i] is unsupported\n", command);
+                    return -1;
+            }
+        break;
+        case 3:
+            int nDeviceGroup = atoi(argv[2]);
+            int nDevice = atoi(argv[3]);
+            int nCommand = atoi(argv[4]);
+            int nAll = 0;
+            if (argc > 4) {
+                nAll = atoi(argv[5]);
+            }
+            boolean bAll = nAll != 0 ? true : false;
+            printf("sending systemCode[%s] unitCode[%i] command[%i] all[%i]\n", nDeviceGroup, nDevice, nCommand, nAll);
+            switch(nCommand) {
+                case 1:
+                    mySwitch.switchOn(nDeviceGroup, nDevice, bAll);
+                    break;
+                case 0:
+                    mySwitch.switchOff(nDeviceGroup, nDevice, bAll);
+                    break;
+                default:
+                    printf("command[%i] is unsupported\n", nCommand);
+                    return -1;
+            }
+    }
+
+
 	return 0;
 }
